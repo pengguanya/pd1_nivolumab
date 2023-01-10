0be468141c261764238cbaa7e71b0f0fb5d7f176
Guanya Peng <guanya.peng24@gmail.com>
2023-01-10 00:21:24 +0000

    fix connection timeout

--- data/download.py
+++ data/download.py
@@ -3,12 +3,15 @@ import requests
 def download_fasta(url: str, filename: str):
     """
     Download FASTA data from a URL and save to a file.
+    Added a timeout and custom headers to help mimic a browser.
     """
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(filename, 'w') as f:
-            f.write(response.text)
-        print(f"Downloaded and saved: {filename}")
-    else:
-        raise Exception(f"Failed to download FASTA from {url}")
-
+    headers = {"User-Agent": "Mozilla/5.0"}
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()  # Raises HTTPError if the HTTP request returned an unsuccessful status code
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"Failed to download FASTA from {url}. Error: {e}")
+    
+    with open(filename, 'w') as f:
+        f.write(response.text)
+    print(f"Downloaded and saved: {filename}")
