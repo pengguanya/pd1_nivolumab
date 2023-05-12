f4f85676d7bece63489df489da717cb6c2f7a79a
Guanya Peng <guanya.peng24@gmail.com>
2023-05-12 21:36:50 +0000

    fix the encoding length

--- models/positional_encoding.py
+++ models/positional_encoding.py
@@ -3,25 +3,30 @@ import torch
 import torch.nn as nn
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000, batch_first: bool = False):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
+        self.batch_first = batch_first
 
         pe = torch.zeros(max_len, d_model)  # (max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)  # (max_len, 1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(1)  # (max_len, 1, d_model)
+        pe[:, 0::2] = torch.sin(position * div_term)  # even indices
+        pe[:, 1::2] = torch.cos(position * div_term)  # odd indices
+        pe = pe.unsqueeze(0)  # (1, max_len, d_model)
+        # Register pe as a buffer, so it's not considered a parameter.
         self.register_buffer('pe', pe)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Args:
-            x: Tensor of shape (seq_len, batch_size, embedding_dim)
+            x: Tensor shape (batch_size, seq_len, d_model) if batch_first=True,
+               or (seq_len, batch_size, d_model) otherwise.
         Returns:
-            Tensor with positional encoding added.
+            x with positional encodings added.
         """
-        x = x + self.pe[:x.size(0)]
+        if self.batch_first:
+            x = x + self.pe[:, :x.size(1)]
+        else:
+            x = x + self.pe.transpose(0, 1)[:x.size(0)]
         return self.dropout(x)
-
